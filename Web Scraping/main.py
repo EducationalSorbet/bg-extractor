c7d647f41c4731f0c974d48a63beb23f8af4d0c1
chirag <chiraggujar21@gmail.com>
2025-11-09 08:19:45 +0000

    Foundation For Fuether improvement prep(IE)

---
+++ Web Scraping/main.py
@@ -0,0 +1,44 @@
+import requests
+from url_parse import urlparse
+from pprint import pprint
+
+# URL of the websites you want to scrape
+url = str(input("Enter URL: "))
+
+# Test the function
+parsed_url = urlparse(url)
+
+# Print out the components
+print("|"+"-"*20+"|")
+for key, value in parsed_url.items():
+    print(f"|  {key.capitalize()}: {value}")
+print("|"+"-"*20+"|")
+
+# from urllib.parse import urlparse
+
+def is_valid_url(url):
+    parsed_url = urlparse(url)
+    # Check if the URL has both a scheme (http/https) and a netloc (domain)
+    return bool(parsed_url["Scheme"]) and bool(parsed_url["netloc"])
+
+
+if is_valid_url(url):
+    print(f"{url} is a valid URL!")
+else:
+    print(f"{url} is not a valid URL!")
+
+try:
+    # Send an HTTP GET request to fetch the source code
+    response = requests.get(url, timeout=10) # Timeout after 10 seconds
+
+    # Check if the request to fetch the source code
+    if response.status_code == 200:
+        # Print the source code of the webpage
+        print(response.text) # This will print the HTML source code of the page
+    else:
+        print(f"Failed to retrive the page. Status code: {respons.status_code}")
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred: {e}")
+
+
+
---
+++ Web Scraping/url_parse.py
@@ -0,0 +1,45 @@
+
+def urlparse(url):
+    # Initialize components to store the parsed results
+    scheme = ""
+    netloc = ""
+    path = ""
+    query = ""
+    fragment = ""
+
+
+    # Step 1: Extract the scheme (protocol)
+    if "://" in url:
+        scheme, url = url.split("://", 1) # Split at '://', scheme is before it, the rest is the URL
+    
+    # Step 2: Extract the netloc (domain)
+    if "/" in url:
+        netloc, url = url.split("/", 1) # Everything before the first slash in the netloc
+    else:
+        netloc = url # If no slash , the entire URL is just the netloc
+    
+    # Step 3: Extract the path (after domain but before query/fragment)
+    if "?" in url:
+        path, url = url.split("/", 1) # Split at '?', path is before it
+    elif "#" in url:
+        path, url = url.split("#", 1) # Split at '#', path is before it
+    else:
+        path = url # If no '?' or '#', everything left is the path
+        url = "" # No query or fragment
+    
+    # Step 4: Extract the query string (if present)
+    if "?" in url:
+        query, url = url.split("#", 1) if "#" in url else (url, "") # Query if before '#' or end of URL
+    
+    # Step 5: Extract the fragment (if present)
+    if "#" in url:
+        fragment = url.split("#", 1)[1] # Everything after '#' is the fragment
+
+    # Return the paarsed URL components
+    return {
+        "Scheme": scheme,
+        "netloc": netloc,
+        "path": path,
+        "query": query,
+        "fragment": fragment
+    } 
